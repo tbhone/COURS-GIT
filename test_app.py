8eecfe58437abc112edf16c8af6b0f9f5b9a9996
Clément Vandewalle <clement.vandewalle@adeo.com>
2024-11-20 12:35:46 +0100

    rename file to lynter

--- test_app.py
+++ test_app.py
@@ -3,12 +3,13 @@ from app import inverser_chaine
 
 class TestInverserChaine(unittest.TestCase):
     def test_inverser_chaine(self):
+        #ici je fais un test qui fonctionne
         self.assertEqual(inverser_chaine("bonjour"), "ruojnob")
         self.assertEqual(inverser_chaine("abcd"), "dcba")
         self.assertEqual(inverser_chaine(""), "")
         self.assertEqual(inverser_chaine("a"), "a")
         self.assertEqual(inverser_chaine("12321"), "12321")
-
+    #ici je fais un test qui ne fonctionne pas
     def test_inverser_chaine(self):
         self.assertEqual(inverser_chaine("bonjour"), "jourbon")
         self.assertEqual(inverser_chaine("abcd"), "dcba")
