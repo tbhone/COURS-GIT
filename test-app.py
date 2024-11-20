c8fca27fa8ab211fc6eaf2f87f58c67e7d12312d
Clément Vandewalle <clement.vandewalle@adeo.com>
2024-11-20 12:05:32 +0100

    add unit-test for my app code

---
+++ test-app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import inverser_chaine
+
+class TestInverserChaine(unittest.TestCase):
+    def test_inverser_chaine(self):
+        self.assertEqual(inverser_chaine("bonjour"), "ruojnob")
+        self.assertEqual(inverser_chaine("abcd"), "dcba")
+        self.assertEqual(inverser_chaine(""), "")
+        self.assertEqual(inverser_chaine("a"), "a")
+        self.assertEqual(inverser_chaine("12321"), "12321")
+
+    def test_inverser_chaine(self):
+        self.assertEqual(inverser_chaine("bonjour"), "jourbon")
+        self.assertEqual(inverser_chaine("abcd"), "dcba")
+        self.assertEqual(inverser_chaine(""), "")
+        self.assertEqual(inverser_chaine("a"), "a")
+        self.assertEqual(inverser_chaine("12321"), "12321")
+
+if __name__ == "__main__":
+    unittest.main()
